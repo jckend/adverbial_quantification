5ebfa6f73489c1fba0196bd2ce9df002335833ef
jckend <142367196+jckend@users.noreply.github.com>
2025-10-20 16:11:55 -0400

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -159,7 +159,7 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
     trial_duration: 10000,
     correct_choice: "a",
     RDK_type: 3, //Applied to all apertures if only one value
-    move_distance: [0,1,1]
+    move_distance: [0, 1, 1],
     aperture_width: 200, //Applied to all apertures if only one value
     number_of_dots: [50, 200, 100], //Different parameter for each aperture. Array length must equal number_of_apertures
     aperture_center_x: [(window.innerWidth/2)-300,window.innerWidth/2,(window.innerWidth/2)+300] //Separate the apertures on the screen (window.innerWidth/2 is the middle of the screen)
