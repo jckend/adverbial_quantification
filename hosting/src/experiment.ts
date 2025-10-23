43c876f407b5b37b39e7a6f4b9bc4598abe5cf59
jckend <142367196+jckend@users.noreply.github.com>
2025-10-23 16:01:52 -0400

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -157,6 +157,7 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
     correct_choice: "a",
     RDK_type: 3, //Applied to all apertures if only one value
     aperture_type: 1,
+    aperture_width: 500,
     move_distance: [0, 1, 1],
     dot_color: ["yellow", "yellow", "blue"],
     aperture_width: 200, //Applied to all apertures if only one value
